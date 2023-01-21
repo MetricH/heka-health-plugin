43cb1a782dd1ea76a09870a35124b0db0b74a75a
Pulkit Goyal <7895pulkit@gmail.com>
2023-01-21 23:07:55 +0530

    add comments to avoid lint errors because of JsonKey

--- lib/connection.dart
+++ lib/connection.dart
@@ -7,11 +7,15 @@ class Connection with _$Connection {
   const Connection._();
   const factory Connection({
     required int id,
+    // ignore: invalid_annotation_target
     @JsonKey(name: 'user_uuid') required String userUuid,
     required String platform,
+    // ignore: invalid_annotation_target
     @JsonKey(name: 'google_fit_refresh_token') String? googleFitRefreshToken,
     required int app,
+    // ignore: invalid_annotation_target
     @JsonKey(name: 'logged_in') required bool loggedIn,
+    // ignore: invalid_annotation_target
     @JsonKey(name: 'last_sync') String? lastSync,
   }) = _Connection;
 
