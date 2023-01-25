cf898e6cdc79e7b9908a34cf24e997c3e065dbe6
Moksh Mahajan <mokshmahajan008@gmail.com>
2023-01-25 09:35:31 +0530

    add watermarks with url launching

--- lib/google_fit_connect/google_fit_connect_button.dart
+++ lib/google_fit_connect/google_fit_connect_button.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:timeago/timeago.dart' as timeago;
+import 'package:url_launcher/url_launcher.dart';
 import 'google_fit_connect_cubit.dart';
 
 class GoogleFitConnectWidget extends StatelessWidget {
@@ -46,106 +47,126 @@ class _GoogleFitConectButtonState extends State<GoogleFitConectButton> {
       builder: (context, state) {
         return Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Card(
-            elevation: 2,
-            child: ListTile(
-              contentPadding: const EdgeInsets.symmetric(horizontal: 16),
-              leading: const Icon(
-                MdiIcons.googleFit,
-                color: Colors.red,
-              ),
-              title: const Text(
-                'Google Fit',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                ),
-              ),
-              subtitle: state.when(
-                error: (error, userUuid) {
-                  return Text(
-                    error.message,
-                    style: const TextStyle(color: Colors.red),
-                  );
-                },
-                initial: (_) => Text(
-                  '',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-                checkingConnection: (_) => Text(
-                  'Checking connection...',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-                noConnection: (_) => Text(
-                  '',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-                tokenInvalidated: (_, __) => Text(
-                  'Disconnected from our systems',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-                makingConnection: (_) => Text(
-                  'Making connection...',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-                connected: (_, __) => Text(
-                  _.lastSync == null
-                      ? 'Last synced 1 min ago'
-                      : 'Last synced ${timeago.format(DateTime.parse(_.lastSync!))}',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-              ),
-              trailing: state.maybeWhen(
-                error: (error, userUuid) => const SizedBox(),
-                checkingConnection: (userUuid) => const SizedBox.square(
-                  dimension: 16,
-                  child: CircularProgressIndicator(
+          child: Stack(
+            children: [
+              Card(
+                elevation: 2,
+                child: ListTile(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+                  leading: const Icon(
+                    MdiIcons.googleFit,
                     color: Colors.red,
                   ),
-                ),
-                makingConnection: (userUuid) => const SizedBox.square(
-                  dimension: 16,
-                  child: CircularProgressIndicator(
-                    color: Colors.red,
-                  ),
-                ),
-                orElse: () => ElevatedButton(
-                  onPressed: state.when(
-                    error: (error, userUuid) => null,
-                    initial: (_) => null,
-                    checkingConnection: (_) => null,
-                    noConnection: (_) =>
-                        context.read<GoogleFitConnectCubit>().createConnection,
-                    tokenInvalidated: (connection, __) => () => context
-                        .read<GoogleFitConnectCubit>()
-                        .connectAgain(connection.id),
-                    makingConnection: (_) => null,
-                    connected: (connection, __) => () => context
-                        .read<GoogleFitConnectCubit>()
-                        .disconnect(connection.id),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    elevation: 1,
-                    backgroundColor: Colors.red,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 24,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24),
+                  title: const Text(
+                    'Google Fit',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
                     ),
                   ),
-                  child: state.when(
+                  subtitle: state.when(
+                    error: (error, userUuid) {
+                      return Text(
+                        error.message,
+                        style: const TextStyle(color: Colors.red),
+                      );
+                    },
+                    initial: (_) => Text(
+                      '',
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                    checkingConnection: (_) => Text(
+                      'Checking connection...',
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                    noConnection: (_) => Text(
+                      '',
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                    tokenInvalidated: (_, __) => Text(
+                      'Disconnected from our systems',
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                    makingConnection: (_) => Text(
+                      'Making connection...',
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                    connected: (_, __) => Text(
+                      _.lastSync == null
+                          ? 'Last synced 1 min ago'
+                          : 'Last synced ${timeago.format(DateTime.parse(_.lastSync!))}',
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                  ),
+                  trailing: state.maybeWhen(
                     error: (error, userUuid) => const SizedBox(),
-                    initial: (_) => const Text(''),
-                    checkingConnection: (_) => const Text('...'),
-                    noConnection: (_) => const Text('Connect'),
-                    tokenInvalidated: (_, __) => const Text('Connect Again'),
-                    makingConnection: (_) => const Text('...'),
-                    connected: (_, __) => const Text('Disconnect'),
+                    checkingConnection: (userUuid) => const SizedBox.square(
+                      dimension: 16,
+                      child: CircularProgressIndicator(
+                        color: Colors.red,
+                      ),
+                    ),
+                    makingConnection: (userUuid) => const SizedBox.square(
+                      dimension: 16,
+                      child: CircularProgressIndicator(
+                        color: Colors.red,
+                      ),
+                    ),
+                    orElse: () => ElevatedButton(
+                      onPressed: state.when(
+                        error: (error, userUuid) => null,
+                        initial: (_) => null,
+                        checkingConnection: (_) => null,
+                        noConnection: (_) => context
+                            .read<GoogleFitConnectCubit>()
+                            .createConnection,
+                        tokenInvalidated: (connection, __) => () => context
+                            .read<GoogleFitConnectCubit>()
+                            .connectAgain(connection.id),
+                        makingConnection: (_) => null,
+                        connected: (connection, __) => () => context
+                            .read<GoogleFitConnectCubit>()
+                            .disconnect(connection.id),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        elevation: 1,
+                        backgroundColor: Colors.red,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 24,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24),
+                        ),
+                      ),
+                      child: state.when(
+                        error: (error, userUuid) => const SizedBox(),
+                        initial: (_) => const Text(''),
+                        checkingConnection: (_) => const Text('...'),
+                        noConnection: (_) => const Text('Connect'),
+                        tokenInvalidated: (_, __) =>
+                            const Text('Connect Again'),
+                        makingConnection: (_) => const Text('...'),
+                        connected: (_, __) => const Text('Disconnect'),
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
+              InkWell(
+                onTap: () {
+                  launch('https://www.hekahealth.co/');
+                },
+                child: const Text(
+                  'Heka',
+                  style: TextStyle(
+                    color: Color(
+                      0xff2351C1,
+                    ),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )
+            ],
           ),
         );
       },
--- lib/ios_connect/ios_health_connect_button.dart
+++ lib/ios_connect/ios_health_connect_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../heka_health_helper.dart';
 import 'ios_connect_cubit.dart';
@@ -57,95 +58,125 @@ class _IosHealthConnectButtonState extends State<IosHealthConnectButton> {
       builder: (context, state) {
         return Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Card(
-            elevation: 2,
-            child: ListTile(
-              contentPadding: const EdgeInsets.symmetric(horizontal: 16),
-              leading: const Icon(
-                MdiIcons.apple,
-                color: Colors.red,
-              ),
-              title: const Text(
-                'Apple HealthKit',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                ),
-              ),
-              subtitle: state.when(
-                error: (error, userUuid) {
-                  return Text(
-                    error.message,
-                    style: const TextStyle(color: Colors.red),
-                  );
-                },
-                initial: (_) => Text(
-                  '',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-                checkingConnection: (_) => Text(
-                  'Checking connection...',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-                noConnection: (_) => Text(
-                  '',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-                makingConnection: (_) => Text(
-                  'Making connection...',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-                syncingData: (_, __) => Text(
-                  '',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-              ),
-              trailing: state.maybeWhen(
-                error: (error, userUuid) => const SizedBox(),
-                checkingConnection: (userUuid) => const SizedBox.square(
-                  dimension: 16,
-                  child: CircularProgressIndicator(
+          child: Stack(
+            children: [
+              Card(
+                elevation: 2,
+                child: ListTile(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+                  leading: const Icon(
+                    MdiIcons.apple,
                     color: Colors.red,
                   ),
-                ),
-                makingConnection: (userUuid) => const SizedBox.square(
-                  dimension: 16,
-                  child: CircularProgressIndicator(
-                    color: Colors.red,
-                  ),
-                ),
-                orElse: () => ElevatedButton(
-                  onPressed: state.when(
-                    error: (error, userUuid) => null,
-                    initial: (_) => null,
-                    checkingConnection: (_) => null,
-                    noConnection: (_) => () => context
-                        .read<IosConnectCubit>()
-                        .createConnection(_syncData),
-                    makingConnection: (_) => null,
-                    syncingData: (_, __) => null,
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    elevation: 1,
-                    backgroundColor: Colors.red,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 24,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24),
+                  title: const Text(
+                    'Apple HealthKit',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
                     ),
                   ),
-                  child: state.when(
+                  subtitle: state.when(
+                    error: (error, userUuid) {
+                      return Text(
+                        error.message,
+                        style: const TextStyle(color: Colors.red),
+                      );
+                    },
+                    initial: (_) => Text(
+                      '',
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                    checkingConnection: (_) => Text(
+                      'Checking connection...',
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                    noConnection: (_) => Text(
+                      '',
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                    makingConnection: (_) => Text(
+                      'Making connection...',
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                    syncingData: (_, __) => Text(
+                      '',
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                  ),
+                  trailing: state.maybeWhen(
                     error: (error, userUuid) => const SizedBox(),
-                    initial: (_) => const Text(''),
-                    checkingConnection: (_) => const Text('...'),
-                    noConnection: (_) => const Text('Connect'),
-                    makingConnection: (_) => const Text('...'),
-                    syncingData: (_, __) => const Text('Connected'),
+                    checkingConnection: (userUuid) => const SizedBox.square(
+                      dimension: 16,
+                      child: CircularProgressIndicator(
+                        color: Colors.red,
+                      ),
+                    ),
+                    makingConnection: (userUuid) => const SizedBox.square(
+                      dimension: 16,
+                      child: CircularProgressIndicator(
+                        color: Colors.red,
+                      ),
+                    ),
+                    orElse: () => ElevatedButton(
+                      onPressed: state.when(
+                        error: (error, userUuid) => null,
+                        initial: (_) => null,
+                        checkingConnection: (_) => null,
+                        noConnection: (_) => () => context
+                            .read<IosConnectCubit>()
+                            .createConnection(_syncData),
+                        makingConnection: (_) => null,
+                        syncingData: (_, __) => null,
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        elevation: 1,
+                        backgroundColor: Colors.red,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 24,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24),
+                        ),
+                      ),
+                      child: state.when(
+                        error: (error, userUuid) => const SizedBox(),
+                        initial: (_) => const Text(''),
+                        checkingConnection: (_) => const Text('...'),
+                        noConnection: (_) => const Text('Connect'),
+                        makingConnection: (_) => const Text('...'),
+                        syncingData: (_, __) => const Text('Connected'),
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
+              Positioned(
+                bottom: 8,
+                right: 8,
+                child: Row(
+                  children: [
+                    Text(
+                      'Powered by ',
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        launch('https://www.hekahealth.co/');
+                      },
+                      child: const Text(
+                        'Heka',
+                        style: TextStyle(
+                          color: Color(
+                            0xff2351C1,
+                          ),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
         );
       },
