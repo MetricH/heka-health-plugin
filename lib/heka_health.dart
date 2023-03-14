dca9a232b2f915fa29be1c2b8e950c5d321d5d84
Pulkit Goyal <7895pulkit@gmail.com>
2023-03-14 16:52:08 +0530

    add a public function to check whether a user is connected or not

--- lib/heka_health.dart
+++ lib/heka_health.dart
@@ -1,1 +1,2 @@
 export 'heka_health_widget.dart';
+export 'utils.dart';
---
+++ lib/utils.dart
@@ -0,0 +1,25 @@
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:heka_health/repository/heka_repository.dart';
+
+import 'constants/platform_name.dart';
+
+class HekaManager {
+  /// Returns whether the user is connected to the given platform or not
+  static Future<bool> isUserConnected(
+      {required String key,
+      required String uuid,
+      required String platforName}) async {
+    var repo = HekaHealth(key);
+    var failureOrSuccess = await repo.fetchConnection(uuid);
+    return failureOrSuccess.fold((error) => false, (connection) async {
+      if (connection == null) {
+        return false;
+      }
+      String? deviceId;
+      if (platforName == PlatformName.appleHealth) {
+        deviceId = (await DeviceInfoPlugin().iosInfo).identifierForVendor;
+      }
+      return connection.isConnected(platforName, deviceId);
+    });
+  }
+}
