4bd561d4becfe8ae36bd200408332abd02fb7b52
Pulkit Goyal <7895pulkit@gmail.com>
2023-04-03 18:44:08 +0530

    replace no internet message with a generic message
    This error can also occur when user does back instead of completing the whole
    Oauth flow. So, let's show a generic message for now.

--- lib/models/heka_health_error.dart
+++ lib/models/heka_health_error.dart
@@ -5,7 +5,7 @@ part 'heka_health_error.freezed.dart';
 class HekaHealthError with _$HekaHealthError {
   const HekaHealthError._();
   const factory HekaHealthError.noConnection(
-      {@Default('Please check your internet!') String message}) = _NoConnection;
+      {@Default('Something went wrong!') String message}) = _NoConnection;
   const factory HekaHealthError.googleClientIdNotRegistered(
           {@Default('Google Client ID not registered!') String message}) =
       _GoogleClientIdNotRegistered;
