6910434747f203eb424e44a76b1c0a620a8ecd47
Pulkit Goyal <7895pulkit@gmail.com>
2023-04-07 15:42:12 +0530

    Revert "upgrade to dio 5.0.0 due to security concerns"
    This breaks end user apps.
    
    This reverts commit 5f981ffa355015ac10cce2abdb48836fcb0ca569.

--- lib/repository/extensions.dart
+++ lib/repository/extensions.dart
@@ -3,5 +3,5 @@ import 'package:dio/dio.dart';
 
 extension DioErrorX on DioError {
   bool get isNoConnectionError =>
-      type == DioErrorType.connectionTimeout && error is SocketException;
+      type == DioErrorType.other && error is SocketException;
 }
