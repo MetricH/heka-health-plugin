b0180c6df7b5ee55cea3e31a5cdd2be070827ff3
Pulkit Goyal <7895pulkit@gmail.com>
2023-10-18 15:48:28 +0530

    introduce few more data types and fix str repr

--- lib/heka_api/types.dart
+++ lib/heka_api/types.dart
@@ -1,10 +1,30 @@
 enum HekaDataType {
   steps,
   calories,
+  distanceMoved,
+  moveMinutes,
+  height,
+  weight,
+  bloodOxygen,
+  restingHeartRate,
+  floorsClimbed,
 }
 
 extension TypeToStr on HekaDataType {
   String toInternalString() {
-    return toString().toLowerCase().split('.').last;
+    switch (this) {
+      case HekaDataType.distanceMoved:
+        return 'distance_moved';
+      case HekaDataType.moveMinutes:
+        return 'move_minutes';
+      case HekaDataType.restingHeartRate:
+        return 'resting_heart_rate';
+      case HekaDataType.floorsClimbed:
+        return 'floors_climbed';
+      case HekaDataType.bloodOxygen:
+        return 'blood_oxygen';
+      default:
+        return toString().toLowerCase().split('.').last;
+    }
   }
 }
